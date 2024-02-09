5fbb3e3553745e435ba3a3a0b59850fbfd596dc0
AliAhmed205 <Ali_lazuli@outlook.com>
2024-02-09 14:05:52 +0100

    New content to add in JSON through Github Link

--- docs/scripts/index.js
+++ docs/scripts/index.js
@@ -10,57 +10,66 @@ document.querySelectorAll('article').forEach(item => {
           }
       })
   })
- updateTitles()
- updateAvatar()
- updateBio()
+  fetchData();
+//  updateTitles()
+//  updateAvatar()
+//  updateBio()
 
 
 
-async function fetchJSONdata() {
+// async function fetchJSONdata() {
+//   try {
+//       const response = await fetch("https://raw.githubusercontent.com/AliAhmed205/web-app-from-scratch-2324/main/docs/scripts/about.json")
+//       const cardsData = await response.json()
+//       return cardsData.cards 
+//   } catch (error) {
+//       console.error('Error fetching JSON data:', error)
+//       return null
+//   }
+// }
+
+// async function updateTitles() {
+//   const cards = await fetchJSONdata()
+//   if (cards) {
+//       const h1Elements = document.querySelectorAll('h1')
+//       h1Elements.forEach((h1, index) => {
+//           h1.textContent = cards[index].title 
+//       })
+//   }
+// }
+
+// async function updateBio() {
+//   const cards = await fetchJSONdata()
+//   if (cards) {
+//       const bioElements = document.querySelectorAll('.bio-El')
+//       bioElements.forEach((p, index) => {
+//         p.textContent = cards.cards[index].bio 
+//     })
+//   }
+// }
+
+
+
+// async function updateAvatar() {
+//   const cards = await fetchJSONdata()
+//   if (cards) {
+//       const avatarEls = document.querySelectorAll('.avatarIcon');
+//       avatarEls.forEach((img, index) => {
+//         if (cards[index] && cards[index].avatar) {
+//           img.src = cards[index].avatar
+//         } else {
+//           img.src = this.avatar;
+//         }
+//       })
+//   }
+// }
+
+async function fetchData() {
   try {
-      const response = await fetch("https://raw.githubusercontent.com/AliAhmed205/web-app-from-scratch-2324/main/docs/scripts/about.json")
-      const cardsData = await response.json()
-      return cardsData.cards 
+    const response = await fetch('https://raw.githubusercontent.com/AliAhmed205/web-app-from-scratch-2324/main/docs/scripts/about.json');
+    const data = await response.json();
+    console.log(data.cards[0]);
   } catch (error) {
-      console.error('Error fetching JSON data:', error)
-      return null
-  }
-}
-
-async function updateTitles() {
-  const cards = await fetchJSONdata()
-  console.log(cards)
-  if (cards) {
-      const h1Elements = document.querySelectorAll('h1')
-      h1Elements.forEach((h1, index) => {
-          h1.textContent = cards[index].title 
-      })
-  }
-}
-
-async function updateBio() {
-  const cards = await fetchJSONdata()
-  console.log(cards)
-  if (cards) {
-      const bioElements = document.querySelectorAll('.bio-El')
-      bioElements.forEach((p, index) => {
-        p.textContent = cards.cards[index].bio 
-    })
-  }
-}
-
-
-
-async function updateAvatar() {
-  const cards = await fetchJSONdata()
-  if (cards) {
-      const avatarEls = document.querySelectorAll('.avatarIcon');
-      avatarEls.forEach((img, index) => {
-        if (cards[index] && cards[index].avatar) {
-          img.src = cards[index].avatar
-        } else {
-          img.src = this.avatar;
-        }
-      })
+    console.error('Error in JSON', error);
   }
 }
