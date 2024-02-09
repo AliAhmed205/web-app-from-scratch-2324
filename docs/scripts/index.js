af6b60cbe694b42aeb5f3cb39715bd3cc35f6ea7
AliAhmed205 <Ali_lazuli@outlook.com>
2024-02-09 06:18:47 +0100

    optimized HTML/CSS structure  + new JS-code.

--- docs/scripts/index.js
+++ docs/scripts/index.js
@@ -1,58 +1,13 @@
-const container = document.querySelector('.container')
-const titleCard = document.querySelector('.title')
-const firstnameElement = document.querySelector('.firstName'); 
-const age = document.querySelector('.ageNumber')
-const bioTextEl = document.querySelector('.bioText')
-const githubJsonUrl = 'https://raw.githubusercontent.com/AliAhmed205/web-app-from-scratch-2324/main/docs/scripts/about.json'
-let isKaartFlipped = false
-
-async function haalJSONop() {
-    try {
-        const reactie = await fetch(githubJsonUrl)
-        const data = await reactie.json()
-        titleCard.textContent = data['kaart-1'].title
-        firstnameElement.textContent = data['kaart-1'].firstname
-        age.textContent = data['kaart-1'].age
-        bioTextEl.textContent = data['kaart-1'].bio
-        const avatarImgs = document.querySelectorAll('.avatarIcon')
-        avatarImgs.forEach(img => {
-            img.src = data['kaart-1'].avatar
-            img.alt = "Pharaoh Avatar"
+document.querySelectorAll('article').forEach(item => {
+    item.addEventListener('click', () => {
+      if (item.classList.contains('flip')) {
+        item.classList.remove('flip')
+      } else {
+        document.querySelectorAll('article').forEach(card => {
+          card.classList.remove('flip')
         })
-        console.log(data)
-    } catch (error) {
-        console.error('Er is een fout opgetreden bij het ophalen en verwerken van de JSON:', error)
-    }
-}
-
-
-haalJSONop()
-
-container.addEventListener('click', function(event) {
-    const clickedCard = event.target.closest('article')
-    const flippedCard = document.querySelector('.container article.flipped')
-    
-    if (flippedCard && clickedCard !== flippedCard) {
-        flippedCard.classList.remove('flipped')
-        isKaartFlipped = false
-    }
-
-    if (clickedCard && !clickedCard.classList.contains('flipped')) {
-        clickedCard.classList.toggle('flipped')
-        isKaartFlipped = true
-    }
-})
-
-document.addEventListener('click', function(event) {
-    if (isKaartFlipped) {
-        const clickedElement = event.target
-        const clickedCard = clickedElement.closest('.container article')
-
-        if (!clickedCard || !clickedElement.closest('.container')) {
-            const flippedCard = document.querySelector('.container article.flipped')
-            flippedCard.classList.remove('flipped')
-            isKaartFlipped = false
-        }
-    }
-})
-
+        item.classList.add('flip')
+      }
+    })
+  })
+  
