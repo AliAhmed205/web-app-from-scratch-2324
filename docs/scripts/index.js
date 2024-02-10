410e798dfb23ab10a3545dd24fd7a53675d0bdc2
AliAhmed205 <Ali_lazuli@outlook.com>
2024-02-10 14:33:26 +0100

    Ext. API (Facts) integrated and JSON translation.

--- docs/scripts/index.js
+++ docs/scripts/index.js
@@ -1,78 +1,127 @@
+const limit = 3;
+
 document.querySelectorAll('article').forEach(item => {
-      item.addEventListener('click', () => {
-          if (item.classList.contains('flip')) {
-              item.classList.remove('flip')
-          } else {
-              document.querySelectorAll('article').forEach(card => {
-                  card.classList.remove('flip')
-              })
-              item.classList.add('flip')
-          }
+  item.addEventListener('click', () => {
+    if (item.classList.contains('flip')) {
+      item.classList.remove('flip')
+    } else {
+      document.querySelectorAll('article').forEach(card => {
+        card.classList.remove('flip')
       })
-  })
-
-  async function fetchData() {
-    try {
-      const response = await fetch('https://raw.githubusercontent.com/AliAhmed205/web-app-from-scratch-2324/main/docs/scripts/about.json');
-      const data = await response.json();
-      
-      // Kaart #1
-      const card1 = document.querySelector('main section:nth-child(1)');
-      if (card1) {
-          const avatarIcon = card1.querySelector('.avatarIcon');
-          if (avatarIcon) {
-              avatarIcon.src = data.cards[0].avatar;
-          }
-          card1.querySelector('.back h1').textContent = data.cards[0].title;
-          card1.querySelector('.back .bio-El').textContent = data.cards[0].bio;
-          if (data.cards[0].icons) {
-              card1.querySelector('.back .linkedin').src = data.cards[0].icons[0];
-              card1.querySelector('.back .github').src = data.cards[0].icons[1];
-              card1.querySelector('.back .discord').src = data.cards[0].icons[2];
-            
-              // Dit werkt niet voor een of andere reden...
-              // card1.querySelector('.back .icon-El:nth-of-type(2)').src = data.cards[0].icons[1];
-              // card1.querySelector('.back .icon-El:nth-of-type(3)').src = data.cards[0].icons[2];
-          }
-      }
-      
-      // Kaart #2
-      const card2 = document.querySelector('main section:nth-child(2)');
-      if (card2) {
-          card2.querySelector('.back img').src = data.cards[1].avatar;
-          card2.querySelector('.back h1').textContent = data.cards[1].title;
-          card2.querySelector('.back .bio-El').textContent = data.cards[1].bio;
-          if (data.cards[1].icons) {
-              card2.querySelectorAll('.icon-El').forEach((icon, index) => {
-                  if (data.cards[1].icons[index]) {
-                      icon.src = data.cards[1].icons[index];
-                  }
-              });
-          }
-      }
-      
-      // Kaart #3
-      const card3 = document.querySelector('main section:nth-child(3)');
-      if (card3) {
-          card3.querySelector('.back img').src = data.cards[2].avatar;
-          card3.querySelector('.back h1').textContent = data.cards[2].title;
-          card3.querySelector('.back .bio-El').textContent = data.cards[2].bio;
-      }
-      
-      // Kaart #4
-      const card4 = document.querySelector('main section:nth-child(4)');
-      if (card4) {
-          card4.querySelector('.front img').src = data.cards[3].avatar;
-          card4.querySelector('.back h1').textContent = data.cards[3].title;
-      }
-      
-      console.log("Data successfully fetched and applied to cards.");
-    } catch (error) {
-      console.error('Error in JSON', error);
+      item.classList.add('flip')
     }
-  }
+  })
+})
 
- // fetchData();
+fetchRandomFacts(1); 
+fetchData()
+
+
+function fetchRandomFacts(limit) {
+  const apiKey = '+1sBxeKIKF0K2wbqzvra6w==MvpkXwEuvs6O7ZhO'; // Jouw API-sleutel hier
+  fetch(`https://api.api-ninjas.com/v1/facts?limit=${limit}`, {
+      method: 'GET',
+      headers: {
+          'X-Api-Key': apiKey,
+          'Content-Type': 'application/json'
+      }
+  })
+  .then(response => {
+      if (!response.ok) {
+          throw new Error('Network response was not ok');
+      }
+      return response.json();
+  })
+  .then(data => {
+      const factsElement = document.getElementById('factElement');
+      factsElement.textContent = ""; // Leeg maken van het element voor het geval er al inhoud in staat
+      data.forEach(fact => {
+          console.log(fact)
+          const paragraph = document.createElement('p');
+          paragraph.textContent = data[0].fact;
+          factsElement.appendChild(paragraph);
+      });
+  })
+  .catch(error => {
+      console.error('Fetch error:', error);
+  });
+}
+
+
+
+
+async function fetchData() {
+  try {
+    const response = await fetch('https://raw.githubusercontent.com/AliAhmed205/web-app-from-scratch-2324/main/docs/scripts/about.json')
+    const data = await response.json()
+
+    // Kaart #1
+    const card1 = document.querySelector('main section:nth-child(1)')
+    if (card1) {
+      const avatarIcon = card1.querySelector('.avatarIcon')
+      if (avatarIcon) {
+        avatarIcon.src = data.cards[0].avatar
+      }
+      card1.querySelector('.back h1').textContent = data.cards[0].title
+      card1.querySelector('.back .bio-El').textContent = data.cards[0].bio
+      if (data.cards[0].icons) {
+        card1.querySelector('.back .linkedin').src = data.cards[0].icons[0]
+        card1.querySelector('.back .github').src = data.cards[0].icons[1]
+        card1.querySelector('.back .discord').src = data.cards[0].icons[2]
+
+        // Dit werkt niet voor een of andere reden...
+        // card1.querySelector('.back .icon-El:nth-of-type(2)').src = data.cards[0].icons[1]
+        // card1.querySelector('.back .icon-El:nth-of-type(3)').src = data.cards[0].icons[2]
+      }
+    }
+
+    // Kaart #2
+    const card2 = document.querySelector('main section:nth-child(2)')
+    if (card2) {
+      card2.querySelector('.back img').src = data.cards[1].avatar
+      card2.querySelector('.back h1').textContent = data.cards[1].title
+      card2.querySelector('.back .bio-El').textContent = data.cards[1].bio
+      if (data.cards[1].icons) {
+        card2.querySelectorAll('.icon-El').forEach((icon, index) => {
+          if (data.cards[1].icons[index]) {
+            icon.src = data.cards[1].icons[index]
+          }
+        })
+      }
+    }
+
+    // Kaart #3
+    const card3 = document.querySelector('main section:nth-child(3)')
+    if (card3) {
+      card3.querySelector('.back img').src = data.cards[2].avatar
+      card3.querySelector('.back h1').textContent = data.cards[2].title
+      card3.querySelector('.back .bio-El').textContent = data.cards[2].bio
+    }
+
+    // Kaart #4
+    const card4 = document.querySelector('main section:nth-child(4)')
+    if (card4) {
+      card4.querySelector('.back img').src = data.cards[3].avatar
+      card4.querySelector('.back h1').textContent = data.cards[3].title
+    }
+
+    console.log("Data successfully fetched and applied to cards.")
+  } catch (error) {
+    console.error('Error in JSON', error)
+  }
+}
+
+
+
+
+
+
+
+
+// OUDE CODE 
+
+
+// fetchData()
 //  updateTitles()
 //  updateAvatar()
 //  updateBio()
@@ -115,12 +164,12 @@ document.querySelectorAll('article').forEach(item => {
 // async function updateAvatar() {
 //   const cards = await fetchJSONdata()
 //   if (cards) {
-//       const avatarEls = document.querySelectorAll('.avatarIcon');
+//       const avatarEls = document.querySelectorAll('.avatarIcon')
 //       avatarEls.forEach((img, index) => {
 //         if (cards[index] && cards[index].avatar) {
 //           img.src = cards[index].avatar
 //         } else {
-//           img.src = this.avatar;
+//           img.src = this.avatar
 //         }
 //       })
 //   }
@@ -128,14 +177,10 @@ document.querySelectorAll('article').forEach(item => {
 
 // async function fetchData() {
 //   try {
-//     const response = await fetch('https://raw.githubusercontent.com/AliAhmed205/web-app-from-scratch-2324/main/docs/scripts/about.json');
-//     const data = await response.json();
-//     console.log(data.cards[0]);
+//     const response = await fetch('https://raw.githubusercontent.com/AliAhmed205/web-app-from-scratch-2324/main/docs/scripts/about.json')
+//     const data = await response.json()
+//     console.log(data.cards[0])
 //   } catch (error) {
-//     console.error('Error in JSON', error);
+//     console.error('Error in JSON', error)
 //   }
 // }
-fetchData();
-
-
-
