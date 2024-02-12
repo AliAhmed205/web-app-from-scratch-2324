bff132aed4a538c154bb671bbcbcef47d348beb9
AliAhmed205 <Ali_lazuli@outlook.com>
2024-02-12 12:53:49 +0100

    Re-activated API and JSON image fixed.

--- docs/scripts/index.js
+++ docs/scripts/index.js
@@ -21,7 +21,7 @@ document.querySelectorAll('article').forEach(item => {
 });
 
 // Function to fetch random facts using an API
-// fetchRandomFacts(1); 
+fetchRandomFacts(1); 
 fetchData();
 
 // Function to fetch data from a JSON API
@@ -90,32 +90,32 @@ function updateCardData(data) {
 }
 
 // Function to fetch random facts using an external API
-// function fetchRandomFacts(limit) {
-//   const apiKey = '+1sBxeKIKF0K2wbqzvra6w==MvpkXwEuvs6O7ZhO'; 
-//   fetch(`https://api.api-ninjas.com/v1/facts?limit=${limit}`, {
-//       method: 'GET',
-//       headers: {
-//           'X-Api-Key': apiKey,
-//           'Content-Type': 'application/json'
-//       }
-//   })
-//   .then(response => {
-//       if (!response.ok) {
-//           throw new Error('Network response was not ok');
-//       }
-//       return response.json();
-//   })
-//   .then(data => {
-//       const factsElement = document.getElementById('factElement');
-//       factsElement.textContent = ""; 
-//       data.forEach(fact => {
-//           console.log(fact)
-//           const paragraph = document.createElement('p');
-//           paragraph.textContent = data[0].fact;
-//           factsElement.appendChild(paragraph);
-//       });
-//   })
-//   .catch(error => {
-//       console.error('Fetch error:', error);
-//   });
-// }
+function fetchRandomFacts(limit) {
+  const apiKey = '+1sBxeKIKF0K2wbqzvra6w==MvpkXwEuvs6O7ZhO'; 
+  fetch(`https://api.api-ninjas.com/v1/facts?limit=${limit}`, {
+      method: 'GET',
+      headers: {
+          'X-Api-Key': apiKey,
+          'Content-Type': 'application/json'
+      }
+  })
+  .then(response => {
+      if (!response.ok) {
+          throw new Error('Network response was not ok');
+      }
+      return response.json();
+  })
+  .then(data => {
+      const factsElement = document.getElementById('factElement');
+      factsElement.textContent = ""; 
+      data.forEach(fact => {
+          console.log(fact)
+          const paragraph = document.createElement('p');
+          paragraph.textContent = data[0].fact;
+          factsElement.appendChild(paragraph);
+      });
+  })
+  .catch(error => {
+      console.error('Fetch error:', error);
+  });
+}
